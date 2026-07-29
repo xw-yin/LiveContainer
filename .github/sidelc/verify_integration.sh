6f6b0d2ecf31f2a2b12de22cbd90b94537893603
yinxinwang <yxw@jygps.com.cn>
2026-07-29 18:31:24 +0800

    ci: guard restored SideStore landscape layout

--- .github/sidelc/verify_integration.sh
+++ .github/sidelc/verify_integration.sh
@@ -29,6 +29,9 @@ require 'override func viewWillTransition(to size:' 'AltStore/Sources/SourcesVie
 require 'layoutConfiguration.contentInsetsReference = .safeArea' 'AltStore/Sources/SourcesViewController.swift'
 require 'layout.sectionInsetReference = .fromSafeArea' 'AltStore/My Apps/MyAppsViewController.swift'
 require 'let safeAreaWidth = collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right' 'AltStore/My Apps/MyAppsViewController.swift'
+require 'override func viewWillTransition(to size:' 'AltStore/My Apps/MyAppsViewController.swift'
 require 'ScrollView(.vertical)' 'AltStore/Settings/WirelessPair/WirelessPairView.swift'
+require 'UIInterfaceOrientationLandscapeLeft' 'AltStore/Info.plist'
+require 'widthSizable="YES" flexibleMaxY="YES"' 'AltStore/Settings/Settings.storyboard'
 
 echo 'LiveContainer SideStore integration checks passed.'
